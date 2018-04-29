ba462deec470de5dd2bc2600f68402cd1cc015b0
eds832 <eds832@gmail.com>
2018-04-29 16:24:08 +0300

    update

--- Tasks/src/by/sardyka/test/task/DivideTask.java
+++ Tasks/src/by/sardyka/test/task/DivideTask.java
@@ -2,23 +2,38 @@ package by.sardyka.test.task;
 
 public class DivideTask {
 	public static void main(String[] arg) {
-		System.out.println("10 / 3 = " + divide(10, 3));
-		System.out.println("-10 / 3 = " + divide(-10, 3));
-		System.out.println("10 / -3 = " + divide(10, -3));
-		System.out.println("10 / 1 = " + divide(10, 1));
-		System.out.println("10 / 100 = " + divide(10, 100));
+		System.out.println("10 / 3 = " + divide(10, 3) + " == " + 10 / 3);
+		System.out.println("-10 / 3 = " + divide(-10, 3) + " == " + (-10 / 3));
+		System.out.println("10 / -3 = " + divide(10, -3) + " == " + 10 / -3);
+		System.out.println("10 / 1 = " + divide(10, 1) + " == " + 10 / 1);
+		System.out.println("10 / 100 = " + divide(10, 100) + " == " + 10 / 100);
+		System.out.println(Integer.MAX_VALUE +  " / 1 = " + divide(Integer.MAX_VALUE, 1) + " == " + Integer.MAX_VALUE / 1);
+		System.out.println((Integer.MIN_VALUE + 1) + " / -1 = " + divide((Integer.MIN_VALUE + 1), -1) + " == " + (Integer.MIN_VALUE + 1) / -1);
+		System.out.println(Integer.MIN_VALUE + " / 1 = " + divide(Integer.MIN_VALUE, 1) + " == " + Integer.MIN_VALUE / 1);
+		System.out.println(Integer.MIN_VALUE + " / -1 = " + divide(Integer.MIN_VALUE, -1) + " == " + Integer.MIN_VALUE / -1);
 		System.out.println("10 / 0 = " + divide(10, 0));
 	}
 
 	static int divide(int a, int b) {
 		if (b == 0) {
-			throw new RuntimeException("b = 0");
+			throw new IllegalArgumentException("b = 0");
 		}
 		int i = 1;
 		boolean sign = false;
 		if (b < 0) {
 			sign = true;
 			b = -b;
+		}
+		if(b == 1) {
+			if(a == Integer.MAX_VALUE) {
+				return a * (sign ? -1 : 1);
+			}
+			if(a == Integer.MIN_VALUE + 1) {
+				return a * (sign ? -1 : 1);
+			}
+			if(a == Integer.MIN_VALUE) {
+				return a;
+			}
 		}
 		if (a < 0) {
 			sign = !sign;
